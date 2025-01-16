5963e68e05ed0aefc70b31e0f92c2fb6d0a63208
SadikovIA <kushimandrous@gmail.com>
2025-01-16 19:39:21 +0300

    дорешал

--- Садиков/ЕГЭ/Марат/15 задание/крылов1вар15.py
+++ Садиков/ЕГЭ/Марат/15 задание/крылов1вар15.py
@@ -1,7 +1,10 @@
-for x in range (1, 1000):
-    for A in range (1, 1000):
-        if (((x % 9 == 0) <= (not(x % 6 == 0))) or (x + A >= 100)) == 1:
-            print (A)
+for A in range (1, 1000):
+    k = 0
+    for x in range (1, 1000):
+        if (((x % 9 == 0) <= (not(x % 6 == 0))) or (x + A >= 100)):
+            k += 1
+        if k == 999:
+            print(A)
             break
 
 
