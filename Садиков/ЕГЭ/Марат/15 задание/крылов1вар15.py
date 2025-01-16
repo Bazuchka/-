580c2f4049a9acb27e2464c3f2bfaba7d0cdcc23
SadikovIA <kushimandrous@gmail.com>
2025-01-16 18:56:00 +0300

    15

--- Садиков/ЕГЭ/Марат/15 задание/крылов1вар15.py
+++ Садиков/ЕГЭ/Марат/15 задание/крылов1вар15.py
@@ -1,1 +1,7 @@
-print('')
+for x in range (1, 1000):
+    for A in range (1, 1000):
+        if (((x % 9 == 0) <= (not(x % 6 == 0))) or (x + A >= 100)) == 1:
+            print (A)
+            break
+
+
