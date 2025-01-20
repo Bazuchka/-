404945604726f76142aec77b43eb5d61e220e784
SadikovIA <kushimandrous@gmail.com>
2025-01-20 23:01:30 +0300

    навел порядок в папке

---
+++ Садиков/Урбан/Модуль_2/module_2_2.py
@@ -0,0 +1,11 @@
+#Введем данные и конвертируем их, т.к. input возвращает string
+first  = int(input('Введите первое число'))
+second = int(input('Введите второе число'))
+third  = int(input('Введите третье число'))
+
+if first == second and first == third and third == second:
+    print("3")
+elif first == second or first == third or third == second:   
+    print("2")
+else: 
+    print("0")
