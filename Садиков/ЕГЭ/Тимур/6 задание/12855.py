b8fbf3aabfbc1aaa637a3484aee984c8c01b1724
SadikovIA <kushimandrous@gmail.com>
2025-01-26 11:00:30 +0300

    26.01 тимур решение

---
+++ Садиков/ЕГЭ/Тимур/6 задание/12855.py
@@ -0,0 +1,7 @@
+s = int(input("Введите переменную s:"))
+t = int(input("Введите переменную t:"))
+#Условие:
+if s < 1 and t < 2:
+    print("YES")
+else:
+    print("NO")
---
+++ Садиков/ЕГЭ/Тимур/6 задание/16014(NEN).py
@@ -0,0 +1,8 @@
+s = int(input("Введите s"))
+t = int(input("Введите t"))
+A = int(input("Введите A"))
+#Условие
+if (s > 10) or (t > A):
+    print("YES")
+else:
+    print("NO")
