9ac3f420db89362f96124e0edef7b58ee8826d97
Timur <chikatilakek@gmail.com>
2025-02-02 12:26:20 +0300

    решил доиа 3 задание

---
+++ Садиков/ЕГЭ/Тимур/3 задание/10639(NEN).py
@@ -0,0 +1,5 @@
+count = 0
+for x in range(0,100):
+    if (not((x <= 14 ) and (x <= 18))) ==1:
+        count += 1
+print(count)
---
+++ Садиков/ЕГЭ/Тимур/3 задание/10868.py
@@ -0,0 +1,5 @@
+a = []
+for x in range(0,100):
+   if ((x >= 6) and not (x > 12)) ==1:
+      a.append(x)
+print(max(a))
