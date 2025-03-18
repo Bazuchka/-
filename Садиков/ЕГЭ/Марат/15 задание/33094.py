1967a145dbea9ace0219fd8d32b3d1be7e9bd79b
SadikovIA <kushimandrous@gmail.com>
2025-03-18 18:40:39 +0300

    п

---
+++ Садиков/ЕГЭ/Марат/15 задание/33094.py
@@ -0,0 +1,7 @@
+for a in range(1, 100):
+    k = 0
+    for x in range(1, 100):
+        if (a < 50) and ((x % a != 0) <= ((x % 10 == 0) <= (x % 18 != 0))):
+            k += 1
+    if k == 99:
+        print(a)
